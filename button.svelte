839eca9ca9c4983558a256220a4290f97e3f5e68
Karoline Avinte <karoline.avinte@itriad.org.br>
2019-10-24 10:22:29 -0400

    Changes in button style

--- button.svelte
+++ button.svelte
@@ -8,7 +8,7 @@ export let hover = "#3f2371";
 export let active = "#26124a";
 
 let isSimple = type === "simple";
-let inline = isSimple && `color: ${color}; border-color: ${color};`;
+let inline = isSimple ? `color: ${color}; border-color: ${color};`:'';
 let bgInitial = isSimple ? 'transparent': color;
 let bg = bgInitial;
 
@@ -19,22 +19,27 @@ let bg = bgInitial;
    text-align: center;
    vertical-align: middle;
    user-select: none;
-   width: 100px;
    box-shadow: 0 0 7px 0 rgba(40, 39, 40, 0.25);
    color: white;
    border:none;
+   font-size: 14px;
+   font-weight: 600;
+   letter-spacing: 0.5px;
   }
 
   .btn-small {
     height: 28px;
+    width: 100px;
   }
 
   .btn-medium {
     height: 38px;
+    width: 100px;
   }
 
   .btn-large {
    height: 48px;
+   width: 140px;
   }
 
   .btn-round {
