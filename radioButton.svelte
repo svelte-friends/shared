8018304b27bc06b6ab4c3052f8af6c27023e7b86
Leandro Amaral <le.amaral@outlook.com>
2019-10-28 14:22:38 -0400

    WC-10 - Send color to radio button

---
+++ radioButton.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { onMount } from "svelte";
+  export let color = "#624695";
+
+  let checkmark;
+
+  onMount(() => {
+    checkmark.style.borderColor = color;
+  });
+</script>
+
+<style>
+  .container {
+    display: block;
+    position: relative;
+    padding-left: 35px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .container input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+  }
+
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 18px;
+    width: 18px;
+    background-color: transparent;
+    border-radius: 50%;
+    border: solid 2px #CECECE;
+  }
+
+  .container input:checked ~ .checkmark {
+    border: solid 6px;
+  }
+</style>
+
+<label class=container>
+  <input type=radio/>
+  <span class=checkmark bind:this={checkmark}/>
+</label>
