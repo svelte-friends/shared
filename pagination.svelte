533413681e97e6b2cd51fec77518372bbff66ad5
Karoline Avinte <karolfurletti@gmail.com>
2019-10-30 15:14:30 -0400

    WC-141-When a pagination has more than 10 pages, it shows as first 9 and shows one last page

--- pagination.svelte
+++ pagination.svelte
@@ -1,18 +1,69 @@
 <script>
-import { onMount } from "svelte";
 export let color = '#624695';
 export let total = 20;
-export let displayRows= 5;
-export let active= 1;
+export let displayRows = 10;
+export let active = 1;
 let previous = "<";
-let startAt = 1;
 let bgActive = `background-color: ${color};`;
-const range =   [...Array(total).keys()].map(i => i + startAt);
 
 const isActive = (index) => {
-   return  active === index;
+   return  parseInt(active) === index;
 };
 
+let paper;
+const getPager = (totalItems, currentPage, pageSize) => {
+
+    let startPage, endPage;
+    const  totalPages = Math.ceil(totalItems / pageSize)
+
+    if (totalPages <= 10) {
+        startPage = 1;
+        endPage = totalPages;
+     } else {
+        if (currentPage <= 6) {
+            startPage = 1;
+            endPage = 10;
+        } else if (currentPage + 4 >= totalPages) {
+            startPage = totalPages - 9;
+            endPage = totalPages
+        } else {
+            startPage = currentPage - 5;
+            endPage = currentPage + 4;
+        }
+    }
+
+    const pages = [...Array((endPage + 1) - startPage).keys()].map(i => startPage + i);
+     paper = {
+        totalItems: totalItems,
+        currentPage: currentPage,
+        pageSize: pageSize,
+        totalPages: totalPages,
+        startPage: startPage,
+        endPage: endPage,
+        pages: pages
+    };
+  };
+
+ getPager(total, active, displayRows);
+
+ const showPage = (item, index) => {
+
+     if(paper.totalPages >=10 && paper.currentPage >=10 && index === 0){
+         return "1..."
+     } else {
+         if(paper.totalPages >=10 && index === 9){
+              return paper.totalPages;
+         } else {
+             if(active === paper.totalPages){
+                 return item;
+             } else if(paper.totalPages > 10 && index === 8) {
+                return item+ '...';
+             } else {
+                 return item;
+             }
+         }
+     }
+ }
 </script>
 
 <style>
@@ -103,14 +154,16 @@ justify-content: center;
   <li class="link"><a href="#">{previous}</a></li>
 {/if}
 
-{#each range as n }
+{#each paper.pages as n, index}
   <li class="item"><a data-index={n}
   class="{isActive(n) && 'active'}"
   style="{isActive(n) && bgActive}"
-  href="#">{n}</a></li>
+  href="#">{showPage(n, index)}
+ </a>
+ </li>
 {/each}
 
-{#if total !== active}
+{#if paper.totalPages  !== active }
   <li class="link"><a href="#"> > </a></li>
 {/if}
 
