7a92dc53a3cc496159069d6314da2b1c37df49c7
Leandro Amaral <le.amaral@outlook.com>
2019-11-04 15:22:36 -0400

    WC-59 - ListItem can receive 'header' property

--- listItem.spec.ts
+++ listItem.spec.ts
@@ -4,14 +4,25 @@ import { render, cleanup } from "@testing-library/svelte";
 import ListItem from "./listItem.svelte";
 
 describe("ListItem Teste Suite", () => {
-  it("ListItem can receive a text", () => {
+  it("ListItem can receive a text property", () => {
     const { getByText } = render(ListItem, {
       props: {
-        text: "Usuário 1",
+        text: "Leandro Amaral",
       }
     });
-    const item = getByText("Usuário 1");
-    expect(item).toHaveTextContent("Usuário 1");
+    const text = getByText("Leandro Amaral");
+    expect(text).toHaveTextContent("Leandro Amaral");
+  });
+
+  it("ListItem can receive a header property", () => {
+    const { getByText } = render(ListItem, {
+      props: {
+        header: "Aluno",
+        text: "Leandro Amaral",
+      }
+    });
+    const header = getByText("Leandro Amaral");
+    expect(header).toHaveTextContent("Leandro Amaral");
   });
 
   afterEach(() => {
