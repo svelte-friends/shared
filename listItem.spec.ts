f7df35178f6060464fc387b3a39cefa9c624c353
Leandro Amaral <le.amaral@outlook.com>
2019-11-05 14:11:14 -0400

    WC-162 - ListItem can receive icon on the right side

--- listItem.spec.ts
+++ listItem.spec.ts
@@ -7,34 +7,34 @@ describe("ListItem Teste Suite", () => {
   it("ListItem can receive a text property", () => {
     const { getByText } = render(ListItem, {
       props: {
-        text: "Leandro Amaral",
+        text: "Some cool text"
       }
     });
-    const text = getByText("Leandro Amaral");
-    expect(text).toHaveTextContent("Leandro Amaral");
+    const text = getByText("Some cool text");
+    expect(text).toHaveTextContent("Some cool text");
   });
 
   it("ListItem can receive a header property", () => {
     const { getByText } = render(ListItem, {
       props: {
         header: "Aluno",
-        text: "Leandro Amaral",
+        text: "Some cool text"
       }
     });
-    const header = getByText("Leandro Amaral");
-    expect(header).toHaveTextContent("Leandro Amaral");
+    const header = getByText("Some cool text");
+    expect(header).toHaveTextContent("Some cool text");
   });
 
   it("ListItem card can receive a color property to set in background", () => {
     const color = "#00c853";
-    const { getByText } = render(ListItem, { 
+    render(ListItem, {
       props: {
         color,
         text: "Some cool text"
       }
     });
-    const component = getByText("Some cool text");
-    expect(component.parentElement).toHaveStyle(`background-color: ${color}`);
+    const component = document.querySelector(".list-item");
+    expect(component).toHaveStyle(`background-color: ${color}`);
   });
 
   afterEach(() => {
