3664685f4e0c80db797659ed4ebc92e623d48069
Fernando Almeida <fernandoalmeid2@hotmail.com>
2019-11-07 15:25:21 -0400

    WC-176 - DotLabel improvements

--- dotLabel.svelte
+++ dotLabel.svelte
@@ -1,22 +1,34 @@
 <script>
-  export let number = "";
-  export let colorText = "#000";
-  export let colorBadge = "#624695";
-  export let textBadgeColor = "white";
-  export let text = "Texto";
-  export let size = "default"; // small
+  export let number = '';
+  export let colorText = '#000';
+  export let colorBadge = '#624695';
+  export let small = false;
+  export let textBadgeColor = 'white';
+  export let text = 'Sample';
+  let badgeClass = '';
 
-  let badgeClass = "";
-  if (number === "") {
-    if (size === "small") badgeClass = "badgeSmall";
-    else badgeClass = "badgeDefault";
-  }
-  if (number.length === 1) badgeClass = "badgeOne";
-  if (number.length > 1) badgeClass = "badgeMore";
-  if (number > 99) number = "99+";
+  if (small && number.length) small = false;
+  if (number.length === 1) badgeClass = 'badgeOne';
+  if (number.length > 1) badgeClass = 'badgeMore';
+  if (number > 99) number = '99+';
 </script>
 
 <style>
+  .content {
+    display: flex;
+    align-items: center;
+  }
+  .dotLabel {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 10px;
+    padding: 3.5px;
+    margin-left: 6px;
+    border-radius: 100%;
+    width: 10px;
+    height: 10px;
+  }
   .textLabel {
     font-family: var(--text-font-family);
     font-size: 14px;
@@ -27,45 +39,31 @@
     letter-spacing: 0.25px;
     margin-left: 6px;
   }
-  .badgeMore {
-    width: 24px;
-    height: 16px;
-    border-radius: 8px;
-  }
   .badgeOne {
     width: 18px;
     height: 18px;
     border-radius: 100%;
   }
-  .badgeSmall {
+  .badgeMore {
+    width: 24px;
+    height: 16px;
+    border-radius: 8px;
+  }
+  .small {
     border-radius: 100%;
     width: 6px;
     height: 6px;
   }
-  .badgeDefault {
-    border-radius: 100%;
-    width: 10px;
-    height: 10px;
-  }
-  .dotLabel {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 10px;
-    padding: 3.5px;
-    margin-left: 6px;
-  }
-  .content {
-    display: flex;
-    align-items: center;
-  }
- 
 </style>
+
 <div class="content">
   <div class="textLabel" style={`color: ${colorText}`}>{text}</div>
   <div
     class="dotLabel {badgeClass}"
+    class:small
     style={`background-color: ${colorBadge}`}>
-    <div style={`color:${textBadgeColor}`}>{number}</div>
+    <div style={`color:${textBadgeColor}`}>
+      {#if number.length}{number}{/if}
+    </div>
   </div>
 </div>
