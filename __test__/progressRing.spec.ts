05b4270c3ee25fd1ec0c43137387079ff59a2f6a
Marco Neves <marco.neves@itriad.org.br>
2019-11-11 15:41:01 -0400

    Progress ring - Type definition (Default, Small and big)

---
+++ __test__/progressRing.spec.ts
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom/extend-expect';
+import 'jest';
+import { render, cleanup } from '@testing-library/svelte';
+import ProgressRing from '../progressRing.svelte';
+
+describe('ProgressRing Test Suite', () => {
+  it('The progress should use 3 sizes: small, medium and large.', () => {
+    const container = document.body;
+    render(ProgressRing);
+    render(ProgressRing, { props: { small: true } });
+    render(ProgressRing, { props: { big: true } });
+
+    const progressDefault = container.querySelector('.progress');
+    const progressSmall = container.querySelector('.small');
+    const progressBig = container.querySelector('.big');
+
+    expect(progressDefault).not.toHaveClass('small');
+    expect(progressDefault).not.toHaveClass('big');
+    expect(progressSmall).toHaveClass('small');
+    expect(progressBig).toHaveClass('big');
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+});
---
+++ progressRing.svelte
@@ -0,0 +1,58 @@
+<script>
+  export let small = false;
+  export let big = false;
+</script>
+
+<style>
+  .progress {
+    width: 32px;
+    height: 32px;
+  }
+
+  .small {
+    width: 46px;
+    height: 46px;
+  }
+
+  .big {
+    width: 60px;
+    height: 60px;
+  }
+
+  .big > .ring {
+    stroke-width: 1.2px;
+  }
+
+  .ring {
+    stroke-width: 1px;
+    stroke-dasharray: 0;
+    stroke-dashoffset: 0;
+    transform-origin: center;
+    stroke: #ccc;
+  }
+  .ring-animation {
+    stroke-width: 3px;
+    stroke-dasharray: 33;
+  }
+</style>
+
+<svg class="progress" class:small class:big viewBox="0 0 32 32">
+  <circle
+    class="ring"
+    fill="none"
+    stroke-width="6"
+    stroke-linecap="round"
+    cx="16"
+    cy="16"
+    r="14" />
+
+  <circle
+    class="ring-animation"
+    fill="none"
+    stroke="#624695"
+    stroke-width="2"
+    stroke-linecap="round"
+    cx="16"
+    cy="16"
+    r="14" />
+</svg>
