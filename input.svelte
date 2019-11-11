9c95afe35b064fedd17ef7e88e593f06eec8edc0
Leandro Amaral <le.amaral@outlook.com>
2019-11-11 11:40:25 -0400

    WC-20 - Input should be receive background color

--- input.svelte
+++ input.svelte
@@ -1,16 +1,21 @@
 <script>
   export let placeholder;
-  export let placeholderColor = "#8c8c8c";
+  export let placeholderColor = '#8c8c8c';
+  export let backgroundColor = '#fff';
 </script>
 
 <style>
   .input {
     width: 100%;
+    background-color: var(--input-background-color);
   }
 
   .input::placeholder {
-    color: var(--placeholder-color);
+    color: var(--input-placeholder-color);
   }
 </style>
 
-<input class="input" {placeholder} style="--placeholder-color: {placeholderColor}"/>
+<input
+  class="input"
+  {placeholder}
+  style="--input-background-color:{backgroundColor}; --input-placeholder-color: {placeholderColor}" />
