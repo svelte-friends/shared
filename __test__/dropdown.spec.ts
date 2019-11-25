9d455d188f1ec8c90b496a5887ef86466e1d5e40
Leandro Amaral <le.amaral@outlook.com>
2019-11-25 11:22:29 -0400

    WC-212 - Dropdown should receive a 'label' property

--- __test__/dropdown.spec.ts
+++ __test__/dropdown.spec.ts
@@ -1,41 +1,18 @@
 import 'jest';
 import '@testing-library/jest-dom/extend-expect';
-import { render, cleanup, fireEvent } from '@testing-library/svelte';
+import { render, cleanup } from '@testing-library/svelte';
 import Dropdown from '../dropdown.svelte';
 
 describe('Dropdown Test Suite', () => {
-  it('Dropdown may be colorized', () => {
-    const container = document.body;
-    render(Dropdown, { props: { color: 'red' } });
-    const select = container.querySelector('select');
-    expect(select).toHaveStyle('background-color:red');
-  });
+  it('Should receive a "label" property', () => {
+    const { getByText } = render(Dropdown, {
+      props: {
+        label: 'Open a dropdown',
+      }
+    });
 
-  it('Dropdown can receive options', () => {
-    let list = [
-      { value: '1', name: 'Option 1' },
-      { value: '2', name: 'Option 2' },
-    ];
-
-    const container = document.body;
-    render(Dropdown, { props: { options: list } });
-    const select = container.querySelector('select');
-    expect(select.childElementCount).toBe(list.length);
-  });
-
-  it('Can receive text color', () => {
-    const container = document.body;
-    render(Dropdown, { props: { color_text: 'blue' } });
-    const select = container.querySelector('select');
-    expect(select).toHaveStyle('color:blue');
-  });
-
-  it('If no text color or background color is passed, assume the default value', () => {
-    const container = document.body;
-    render(Dropdown);
-    const select = container.querySelector('select');
-    expect(select).toHaveStyle('color:#0b0e1e');
-    expect(select).toHaveStyle('background-white:');
+    const label = getByText('Open a dropdown');
+    expect(label).toBeInTheDocument();
   });
 
   afterEach(() => {
