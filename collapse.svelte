a3ac9158ba091b275f31857651313c8f2bf6eb9c
Fernando Almeida <fernandoalmeid2@hotmail.com>
2019-11-26 13:49:24 -0400

    WC-63 - Add color to the button

--- collapse.svelte
+++ collapse.svelte
@@ -1,31 +1,31 @@
 <script>
   export let label = '';
+  export let color = '#fff';
   let arrow = `<svg width="16" height="16" viewBox="0 0 16 16">
                 <path fill="#1A051D" fill-rule="nonzero" d="M3.415 6.65l3.826 4.464a1 1 0 0 0 1.518 0l3.826-4.463A1 1 0 0 0 11.825 5h-7.65a1 1 0 0 0-.76 1.65z"/>
                </svg>`;
+
 </script>
 
 <style>
-  .collapse-label {
+  .collapse {
     display: flex;
-    justify-content: center;
+    height: 50px;
+    width: 100%;
+    justify-content: space-between;
+    background-color: var(--collapse-background);
+    padding: 15px;
+  }
+  .item {
+    display: flex;
     align-items: center;
-    cursor: pointer;
-    padding: 0 5px;
   }
-  .collapse-button {
-    height: 30px;
-    font-size: 14px;
-    font-weight: 500;
-    float: left;
-    border: none;
-    background-color: transparent;
-  }
+
 </style>
 
-<div class="collapse">
-  <div class="collapse-label">
-    <button class="collapse-button">{label}</button>
+<div
+  class="collapse"
+  style="--collapse-background:{color}">
+  <div class="item collapse-label">{label}</div>
     {@html arrow}
   </div>
-</div>
