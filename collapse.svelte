de55b27854e88060974040196c40c7c3ea521e5c
Fernando Almeida <fernandoalmeid2@hotmail.com>
2019-11-27 16:19:23 -0400

    WC-96 - Add element in collapse component

--- collapse.svelte
+++ collapse.svelte
@@ -1,41 +1,59 @@
 <script>
   export let label = '';
+  export let element = '';
   export let color = '#fff';
   let show = false;
-  let animation = false;
+  let open = false;
+  let close = false;
   let arrow = `<svg width="16" height="16" viewBox="0 0 16 16">
                 <path fill="#1A051D" fill-rule="nonzero" d="M3.415 6.65l3.826 4.464a1 1 0 0 0 1.518 0l3.826-4.463A1 1 0 0 0 11.825 5h-7.65a1 1 0 0 0-.76 1.65z"/>
                </svg>`;
   function handleClick() {
     show = !show;
-    animation = !show;
+    open = !open;
+    close = !open;
   }
 </script>
 
 <style>
   .collapse {
-    display: flex;
-    height: 50px;
     width: 100%;
+    position: relative;
+  }
+
+  .collapse-label {
+    display: flex;
     justify-content: space-between;
+    align-items: center;
+    width: 100%;
     background-color: var(--collapse-background);
+    height: 50px;
     padding: 15px;
   }
-  .item {
+
+  .collapse-button {
     display: flex;
-    align-items: center;
   }
-  .collapse-button.animation {
-    animation: rotationClose 0.5s;
+
+  .open {
+    animation: rotationOpen 0.5s forwards;
   }
+
+  .close {
+    animation: rotationClose 0.5s forwards;
+  }
+
+  .collapse-content {
+    display: none;
+    width: 100%;
+    position: absolute;
+  }
+
   .show {
-    transform: rotate(180deg);
-    animation: rotationShow 0.5s;
+    display: block;
   }
-  @keyframes rotationShow {
-    from {
-      transform: rotate(0deg);
-    }
+
+  @keyframes rotationOpen {
     to {
       transform: rotate(180deg);
     }
@@ -54,8 +72,13 @@
   on:click={handleClick}
   class="collapse"
   style="--collapse-background:{color}">
-  <div class="item collapse-label">{label}</div>
-  <div class="item collapse-button" class:show class:animation>
-    {@html arrow}
+  <div class="collapse-label">
+    <span>{label}</span>
+    <div class="collapse-button" class:open class:close>
+      {@html arrow}
+    </div>
+  </div>
+  <div class="collapse-content" class:show>
+    <slot />
   </div>
 </div>
