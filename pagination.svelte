d3599274a6439f8fa0cb2576177f534e30844652
Leandro Amaral <le.amaral@outlook.com>
2019-11-28 11:23:09 -0400

    WC-210 - Pagination improvements

--- pagination.svelte
+++ pagination.svelte
@@ -11,14 +11,6 @@
   let el;
   let pager;
 
-  const classInline = index => {
-    return parseInt(active) === index ? 'active' : '';
-  };
-
-  const styleInline = index => {
-    return parseInt(active) === index ? 'color:white' : '';
-  };
-
   const getPager = (totalItems, currentPage, pageSize) => {
     let startPage, endPage;
     const totalPages = Math.ceil(totalItems / pageSize);
@@ -96,60 +88,57 @@
 </script>
 
 <style>
-  ul {
-    padding-left: 0px;
-    height: 30px;
-    flex-direction: row;
-    align-items: center;
-    text-align: center;
-    width: auto;
+  .pagination {
     display: flex;
-    justify-content: center;
+    height: 30px;
   }
 
-  .item {
+  .pagination-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
     cursor: pointer;
     width: 30px;
-    height: 30px;
     font-size: 14px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: 0.25px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     background-color: #efefef;
-    text-decoration: none;
   }
 
-  .animation {
+  .pagination-item div {
+    width: 100%;
+    height: 100%;
+    position: absolute;
     border-radius: 10px;
+    z-index: 0;
     transform: scale(0);
     transition: transform 0.3s;
-    width: 30px;
-    height: 30px;
-    position: absolute;
   }
 
-  .active {
-    width: 30px;
-    height: 30px;
-    border-radius: 10px;
+  .pagination-item span {
+    color: #8c8c8c;
+    z-index: 1;
+  }
+
+  .pagination-item:nth-child(2) {
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+
+  .pagination-item:nth-last-child(2) {
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+  }
+
+  .active > div {
     box-shadow: 0 1px 2px 0 rgba(40, 39, 40, 0.3);
-    padding-top: 6px;
     transform: scale(1);
   }
 
-  span {
-    text-decoration: none;
-    text-align: center;
-    color: #8c8c8c;
-    z-index: 2;
+  .active > span {
+    color: #fff;
   }
 
-  .link {
+  .pagination-arrow {
     background-color: #efefef;
     color: #8c8c8c;
     width: 30px;
@@ -161,53 +150,42 @@
     cursor: pointer;
   }
 
+  .pagination-arrow:first-child {
+    margin-right: 10px;
+  }
+
+  .pagination-arrow:last-child {
+    margin-left: 10px;
+  }
+
   .hidden {
     visibility: hidden;
     cursor: none;
   }
-
-  .link:first-child {
-    margin-right: 10px;
-  }
-
-  .link:last-child {
-    margin-left: 10px;
-  }
-
-  .item:nth-child(2) {
-    border-top-left-radius: 15px;
-    border-bottom-left-radius: 15px;
-  }
-
-  .item:nth-last-child(2) {
-    border-top-right-radius: 15px;
-    border-bottom-right-radius: 15px;
-  }
 </style>
 
-<ul class="container" bind:this={el}>
+<ul class="pagination" bind:this={el}>
   <li
     class:hidden={active === 1}
     on:click={() => setPage(active - 1)}
-    class="link">
+    class="pagination-arrow">
     <span>{previous}</span>
   </li>
 
   {#each pager.pages as n, index}
-    <li on:click={() => setPage(n, index)} class="item">
-      <span
-        data-index={n}
-        style="background-color:{color}"
-        class="animation {classInline(n)}" />
-      <span style={styleInline(n)}>{showPage(n, index)}</span>
+    <li
+      class:active={parseInt(n) === active}
+      class="pagination-item"
+      on:click={() => setPage(n, index)}>
+      <div style="background-color: {color}" data-index={n} />
+      <span>{showPage(n, index)}</span>
     </li>
   {/each}
 
   <li
     class:hidden={pager.totalPages === active}
     on:click={() => setPage(active + 1)}
-    class="link">
+    class="pagination-arrow">
     <span>{next}</span>
   </li>
-
 </ul>
