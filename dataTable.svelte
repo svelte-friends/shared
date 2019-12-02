a68b24044b6fe09cef6f191682d1ff2a95d869f6
marco.neves@itriad.org.br <marcobneves@gmail.com>
2019-12-02 15:30:11 -0400

    WC - 239 - Receive items content

--- dataTable.svelte
+++ dataTable.svelte
@@ -1,7 +1,17 @@
 <script>
   export let col = 4;
+  let columnTotal = [];
   export let thead = ['Column 1', 'Column 2', 'Column 3', 'Column 4'];
-  export let tbody = [];
+  export let tbody = [
+    ['Test 1', 'Test 2', 'Test 3', 'Test 4'],
+    ['Test 1', 'Test 2', 'Test 3', 'Test 4'],
+    ['Test 1', 'Test 2', 'Test 3', 'Test 4'],
+  ];
+  export let tbodyComponent = [];
+
+  for (let i = 0; i < col; i++) {
+    columnTotal.push(i);
+  }
 </script>
 
 <style>
@@ -28,6 +38,27 @@
     display: flex;
     flex-direction: column;
   }
+
+  .tbody-content {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+  }
+  .tbody-row {
+    display: flex;
+  }
+  .tbody-column {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    /* background-color: #ccc; */
+    justify-items: flex-start;
+    padding: 20px;
+  }
+  .tbody-content .tbody-row:nth-child(even) {
+    background-color: #f2f2f2;
+  }
 </style>
 
 <div class="content">
@@ -39,7 +70,25 @@
   </div>
 
   <div class="tbody">
-    <div>Conteudo</div>
+    {#each columnTotal as colIndex}
+      <svelte:component this={tbodyComponent[colIndex]} />
+    {/each}
+  </div>
+
+  <div class="tbody-content">
+    {#each tbody as array}
+      <div class="tbody-row">
+        {#each array as item}
+          <div class="tbody-column">
+            {#if typeof item === 'string'}
+              {item}
+            {:else}
+              <svelte:component this={item} />
+            {/if}
+          </div>
+        {/each}
+      </div>
+    {/each}
   </div>
 
 </div>
