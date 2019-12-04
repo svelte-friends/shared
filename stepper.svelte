fc3806577270f417870df9ce1a66ffb0e51687a0
Fernando Almeida <fernandoalmeid2@hotmail.com>
2019-12-04 18:07:57 -0400

    WC-249 - Add separator in steps component

--- stepper.svelte
+++ stepper.svelte
@@ -1,8 +1,14 @@
 <script>
   export let labelColor = '';
   export let steps = ['step1', 'step2', 'step3', 'step4'];
-  export let stepColor = '';
-  export let numberColor = '';
+  export let stepColor = '#624695';
+  export let numberColor = '#fff';
+
+  const styleColor = ` 
+  --stepper-label-color: ${labelColor}; 
+  --stepper-circle-color: ${stepColor};
+  --stepper-number-color: ${numberColor};
+  `;
 </script>
 
 <style>
@@ -19,6 +25,7 @@
     align-items: center;
     justify-content: space-around;
     width: 100%;
+    position: relative;
   }
 
   .step-circle {
@@ -36,14 +43,26 @@
 
   .step-label {
     margin: 10px 0;
+    font-weight: bold;
     color: var(--stepper-label-color);
+  }
+
+  .step::after {
+    position: absolute;
+    content: '';
+    height: 4px;
+    width: 50%;
+    top: 25%;
+    right: -25%;
+    background-color: rgba(0, 0, 0, 0.2);
+    border-radius: 2px;
+  }
+  .step:last-child::after {
+    content: none;
   }
 </style>
 
-<ul
-  class="stepper"
-  style=" --stepper-label-color:{labelColor}; --stepper-circle-color:{stepColor};
-  --stepper-number-color:{numberColor}">
+<ul class="stepper" style={styleColor}>
   {#each steps as step, index}
     <li class="step">
       <div class="step-circle">{index + 1}</div>
